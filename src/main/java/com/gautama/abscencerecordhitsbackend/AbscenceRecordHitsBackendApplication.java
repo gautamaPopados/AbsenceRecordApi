076dac1da45c8d2fe8f27574fd3db9e4a80a0fc4
Egor Morozov <egornexus10@gmail.com>
2025-02-20 18:49:17 +0700

    initialized by spring boot

---
+++ src/main/java/com/gautama/abscencerecordhitsbackend/AbscenceRecordHitsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gautama.abscencerecordhitsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbscenceRecordHitsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbscenceRecordHitsBackendApplication.class, args);
+    }
+
+}
