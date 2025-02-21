3f1441ac6fc242afe2cf57275e1c85abb62218c7
Pavel Khorev <khorev05@list.ru>
2025-02-21 21:14:00 +0700

    Added User and Request classes

---
+++ src/main/java/com/gautama/abscencerecordhitsbackend/core/model/User.java
@@ -0,0 +1,25 @@
+package com.gautama.abscencerecordhitsbackend.core.model;
+
+import com.gautama.abscencerecordhitsbackend.api.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserRole userRole;
+    private boolean isAdmin;
+}
