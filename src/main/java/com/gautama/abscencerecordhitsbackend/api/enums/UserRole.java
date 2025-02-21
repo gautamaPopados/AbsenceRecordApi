5c2426cd21cc166eddbcb6797098be9ab4079e0b
Egor Morozov <egornexus10@gmail.com>
2025-02-21 20:13:23 +0700

    added UserRole enum

---
+++ src/main/java/com/gautama/abscencerecordhitsbackend/api/enums/UserRole.java
@@ -0,0 +1,17 @@
+package com.gautama.abscencerecordhitsbackend.api.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    DEANERY("Деканат"),
+    TEACHER("Преподаватель"),
+    STUDENT("Студент");
+
+    private final String displayName;
+
+    UserRole(String displayName) {
+        this.displayName = displayName;
+    }
+
+}
